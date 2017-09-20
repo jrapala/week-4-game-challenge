279c75b17ec48d370d02045489157485cdcf24a8
Juliette Rapala <jrapala@gmail.com>
2017-09-19 22:09:47 -0500

    Removing object orientated programming. Game now stores player choices for character and defender.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,112 +3,98 @@
 
 $(document).ready(function(){
 
-		var starWarsApp = {
+		// Variables 
+		// =====================================================================================
 
-			// Variables 
-			// =====================================================================================
-
-			userCharacter : '',
-			currentDefender : '',
-			characters : {
-				obiwan : {
-					name : 'Obi-Wan Kenobi',
-					photo : 'assets/images/obiwankenobi.jpg',
-					alive : true,
-					hp : 120,
-					attack : 8,
-					counterAttack : 24
-				},
-
-				luke : {
-					name : 'Luke Skywalker',
-					photo : 'assets/images/lukeskywalker.jpg',
-					alive : true,
-					hp : 100,
-					attack : 10,
-					counterAttack : 5 
-				},
-
-				darthsid : {
-					name : 'Darth Sidious',
-					photo : 'assets/images/darthsidious.jpg',
-					alive : true,
-					hp : 150,
-					attack : 10,
-					counterAttack : 20
-				},
-
-				darthmaul : {
-					name : 'Darth Maul',
-					photo : 'assets/images/darthmaul.jpg',
-					alive : true,
-					hp : 180,
-					attack : 12,
-					counterAttack : 25
-				}
+		var userCharacter = '';
+		var currentDefender = '';
+		var isCharacterChosen = false;
+		var isDefenderChosen = false;		
+		var characters = {
+			obiwan : {
+				name : 'Obi-Wan Kenobi',
+				photo : 'assets/images/obiwankenobi.jpg',
+				alive : true,
+				hp : 120,
+				attack : 8,
+				counterAttack : 24
 			},
 
-			// Functions
-			// =====================================================================================	
-			//
-			// Initialize game
-			init : function() {
-				for (var key in this.characters) {
-					var $characterDiv = $('<div>');
-					$characterDiv.addClass("img-white-wrapper choice");
-					$characterDiv.attr("value", key);
-					var $characterImg = $('<img>');
-					$characterImg.addClass("img");
-					$characterImg.attr("src", this.characters[key].photo);
-					var $characterName = this.characters[key].name;
-					var $characterHP = this.characters[key].hp;
-					$characterDiv.append($characterName);
-					$characterDiv.append($characterImg);
-					$characterDiv.append($characterHP);
-					$('#characterChoice').append($characterDiv);
-				}
-				this.chooseCharacter();
+			luke : {
+				name : 'Luke Skywalker',
+				photo : 'assets/images/lukeskywalker.jpg',
+				alive : true,
+				hp : 100,
+				attack : 10,
+				counterAttack : 5 
 			},
 
-			// On click, character becomes your character
-			chooseCharacter : function() {
-				var self = this;
-				$('.choice').on("click", function() {
-					// Set userCharacter variable to name of character clicked on
-					self.userCharacter = $(this).attr("value");
-					// Remove "choice" class from character clicked on
-					$(this).removeClass("choice");
-					// Move character clicked on to "Your Character" section
-					$(this).appendTo($('#player'));
-					// Hide row of choices
-					$('#characterChoice').hide();
-					// Move remaining characters to "Enemies Available to Attack"
-					$('.choice').appendTo($('#enemies'));
-					// Change background color of enemies and add enemy class
-					$('.choice').addClass('img-red-wrapper enemy');
-					// Remove old background color and choice class
-					$('.choice').removeClass('img-white-wrapper choice');
-					self.chooseDefender();
-				});
+			darthsid : {
+				name : 'Darth Sidious',
+				photo : 'assets/images/darthsidious.jpg',
+				alive : true,
+				hp : 150,
+				attack : 10,
+				counterAttack : 20
 			},
 
-			chooseDefender : function () {
-				var self = this;
-				$('.enemy').on("click", function() {
-					console.log("hello?");
-					self.currentDefender = $(this).attr("value");
-					$(this).removeClass("enemy");
-					$(this).appendTo($('#defender'));
-					$(this).addClass('img-green-wrapper defender');
-					$(this).removeClass('img-white-wrapper');
-					self.fight();
-				});
-			},
-
-			fight : function () {
-
+			darthmaul : {
+				name : 'Darth Maul',
+				photo : 'assets/images/darthmaul.jpg',
+				alive : true,
+				hp : 180,
+				attack : 12,
+				counterAttack : 25
 			}
+		}
 
+		// Functions
+		// =====================================================================================	
+
+		// Initialize game
+		function init() {
+			// For each character in characters object..
+			for (var key in characters) {
+				// Create character div
+				var $characterDiv = $('<div>');
+				// Add white background color class and Choice class
+				$characterDiv.addClass("img-white-wrapper choice");
+				// Add a value attribute containing the name of the object
+				$characterDiv.attr("value", key);
+				// Create image tag
+				var $characterImg = $('<img>');
+				// Add img class
+				$characterImg.addClass("img");
+				// Add source code of character image
+				$characterImg.attr("src", characters[key].photo);
+				// Get name from character object
+				var $characterName = characters[key].name;
+				// Get HP from character object
+				var $characterHP = characters[key].hp;
+				// Append name, image, and HP to character div
+				$characterDiv.append($characterName);
+				$characterDiv.append($characterImg);
+				$characterDiv.append($characterHP);
+				// Append character div to top row of screen
+				$('#characterChoice').append($characterDiv);
+			};
+		};			
+		
+		// On click, character becomes your character
+
+
+
+		// // If a character has not been chosen yet..
+		// if (!isCharacterChosen) {
+		// 	console.log("A character needs to be chosen.");
+		// 	// Run choose character function
+		// 	chooseCharacter();
+		// };
+		// if (isCharacterChosen && !isDefenderChosen) {
+		// 	console.log("A defender needs to be chosen.");
+		// 	// Run choose character function
+		// 	chooseDefender();
+		// }; 	
 
 			//
 			// Pick enemy
@@ -126,11 +112,44 @@ $(document).ready(function(){
 			// If you are alive, you win!
 			// If you are dead, you lose!
 
-	}
+	
 	// Gameplay
 	// =====================================================================================
 
-starWarsApp.init();
+	init();
+	$('.choice').on("click", function() {
+		if (isCharacterChosen) return;
+		// Set userCharacter variable to name of character clicked on
+		userCharacter = $(this).attr("value");
+		isCharacterChosen = true;
+		console.log("User chose " + userCharacter + " as their character.");
+		// Remove "choice" class from character clicked on
+		$(this).removeClass("choice");
+		// Move character clicked on to "Your Character" section
+		$(this).appendTo($('#player'));
+		// Hide row of choices
+		$('#characterChoice').hide();
+		// Move remaining characters to "Enemies Available to Attack"
+		$('.choice').appendTo($('#enemies'));
+		// Change background color of enemies and add enemy class
+		$('.choice').addClass('img-red-wrapper enemy');
+		// Remove old background color and choice class
+		$('.choice').removeClass('img-white-wrapper choice');
+		$('.enemy').on("click", function() {
+			//if (isDefenderChosen) return;
+			currentDefender = $(this).attr("value");
+			isDefenderChosen = true;
+			console.log("User chose " + currentDefender + " as their defender.");
+			$(this).removeClass("enemy");
+			$(this).appendTo($('#defender'));
+			$(this).addClass('img-green-wrapper defender');
+			$(this).removeClass('img-white-wrapper');
+		});
+	});
+	fight();
+
+
+
 });
 
 // Notes:
